a4cf0b98a56d7fa9910ba869e33c529596ded2f3
sulaimanalamsyah <sulaimana182@gmail.com>
2023-11-16 14:28:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CRUDprak',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;

efbcaf0cb01a5abcac41bdbc6191caa0aa7dccaa
sulaimanalamsyah <sulaimana182@gmail.com>
2023-11-22 23:00:32 +0700

    tugas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'mahasiswa',
+    pathMatch: 'full'
+  },
+  {
+    path: 'mahasiswa',
+    loadChildren: () => import('./mahasiswa/mahasiswa.module').then( m => m.MahasiswaPageModule)
   }
 ];
 @NgModule({

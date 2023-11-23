b225f5f41467aab449a61ad597dd521c681ebf75
sulaimanalamsyah <sulaimana182@gmail.com>
2023-11-23 21:14:15 +0700

    lagi

---
+++ apiCoba/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+//header untuk menangani cors policy
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
+header('Content-Type: application/json, charset=utf-8');
+//membuat variable koneksi ke mysql
+$koneksi = mysqli_connect('localhost','root','','sinau') or die ('koneksi gagal');
